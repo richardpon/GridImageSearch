3b8f15a433ed837f6078ae2a2d2f34c5ecc73bc4
Richard Pon <richard.pon@gmail.com>
2015-05-16 12:34:40 -0700

    Custom layout and adapter

--- app/src/main/java/com/codepath/gridimagesearch/images/ImageResultsAdapter.java
+++ app/src/main/java/com/codepath/gridimagesearch/images/ImageResultsAdapter.java
@@ -1,9 +1,16 @@
 package com.codepath.gridimagesearch.images;
 
 import android.content.Context;
+import android.text.Html;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.gridimagesearch.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -11,11 +18,28 @@ public class ImageResultsAdapter extends ArrayAdapter<ImageResult> {
 
 
     public ImageResultsAdapter(Context context, List<ImageResult> images) {
-        super(context, android.R.layout.simple_list_item_1, images);
+        super(context, R.layout.item_image_result, images);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        ImageResult imageInfo = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_image_result, parent, false);
+        }
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+
+        //Clear out the image
+        ivImage.setImageResource(0);
+
+        //populate
+        //tvTitle.setText(imageInfo.title);
+        tvTitle.setText(Html.fromHtml(imageInfo.title));
+
+        // Remotely download image
+        Picasso.with(getContext()).load(imageInfo.thumbUrl).into(ivImage);
+        return convertView;
     }
 }
